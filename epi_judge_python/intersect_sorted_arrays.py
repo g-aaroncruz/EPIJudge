579e812922d73ede0d6221b96997253e9f71fcfb
Aaron Cruz <g.aaroncruz@gmail.com>
2019-06-24 17:37:17 -0700

    sorting : 13.1, intersection of sorted arrays * O(N) run-time; O(N) space

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -2,8 +2,31 @@ from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A, B):
-    # TODO - you fill in here.
-    return []
+    # O (n) time O (N) space
+    i_A = 0
+    i_B = 0
+
+    intersection = []
+    while i_A < len(A) and i_B < len(B):
+        if A[i_A] > B[i_B]:
+            # increment B pointer
+
+            i_B += 1
+        elif A[i_A] < B[i_B]:
+            # increment A pointer
+
+            i_A += 1
+        else:
+            # values are equal to each other can use either A or B
+
+            if A[i_A] not in intersection:
+                intersection.append(A[i_A])
+
+            # increment both counters
+            i_A += 1
+            i_B += 1
+
+    return intersection
 
 
 if __name__ == '__main__':
